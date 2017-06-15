d067323fa8082ca08eb3cf6801cf5f1bc6db2a96
Aidan Gadberry <agadberr@gmail.com>
2017-06-14 20:16:07 -0700

    beginning Facebook auth

---
+++ PenPals/PenPals/LandingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LandingViewController.swift
+//  PenPals
+//
+//  Created by Aidan Gadberry on 6/14/17.
+//  Copyright © 2017 Aidan Gadberry. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
