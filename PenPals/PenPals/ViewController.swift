2c4b31055d3132843867611e1ae2980a758652d0
Aidan Gadberry <agadberr@gmail.com>
2017-06-14 19:11:45 -0700

    base iOS application

---
+++ PenPals/PenPals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PenPals
+//
+//  Created by Aidan Gadberry on 6/14/17.
+//  Copyright © 2017 Aidan Gadberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
